1a03d7b2746a926d9b464ae6fbfb78a6c9ac3e0b
Kal EL Pratama <152152274+kalpratama@users.noreply.github.com>
2024-08-06 17:51:01 +0700

    UI
    ^

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
@@ -8,12 +9,23 @@ public class PlayerHealth : MonoBehaviour
     public float maxHealth = 100f;
     private float currentHealth;
     public Slider healthBar;
+    public GameObject gameOverScreen;
+    public Text killCountText;
+    public Text timeSpentText;
+
+    private float timeSpent;
+    private int killCount;
 
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.maxValue = maxHealth;
         healthBar.value = currentHealth;
+    }
+
+    void Update()
+    {
+        timeSpent += Time.deltaTime;    
     }
 
     public void TakeDamage(float amount)
@@ -29,7 +41,28 @@ public class PlayerHealth : MonoBehaviour
 
     void Die()
     {
-        // Implement game over logic here
         Debug.Log("Player died");
+        SceneManager.LoadScene("2GameOver");
+        gameOverScreen.SetActive(true);
+        Time.timeScale = 0f;
+        killCountText.text = "Kills: " + killCount;
+        timeSpentText.text = "Time Spent: " + Mathf.FloorToInt(timeSpent) + "s";
+    }
+
+    public void IncreaseKillCount()
+    {
+        killCount++;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("0MainMenu");
     }
 }
